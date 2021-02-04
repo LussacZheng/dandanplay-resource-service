b91f29327a6a52fa5dde983d9727140c222848d2
Lussac Zheng <lussaczheng@gmail.com>
2021-02-04 22:06:10 +0800

    chore(go/.dist): update the build scripts

--- golang/.dist/build.cmd
+++ golang/.dist/build.cmd
@@ -8,21 +8,24 @@ setlocal
 
 set "name=dandanplay-resource-service"
 
-cd "%~dp0.."
+pushd "%~dp0.."
 
 echo  * go building...
 
 set "GOOS=windows" & set "GOARCH=amd64"
-go build -ldflags="-s -w" -o .dist/%name%.%GOOS%.%GOARCH%.exe
+go build -ldflags="-s -w" -o .dist/%name%.win64.exe
 
 set "GOOS=windows" & set "GOARCH=386"
-go build -ldflags="-s -w" -o .dist/%name%.%GOOS%.%GOARCH%.exe
+go build -ldflags="-s -w" -o .dist/%name%.win32.exe
 
 set "GOOS=linux" & set "GOARCH=amd64"
-go build -ldflags="-s -w" -o .dist/%name%.%GOOS%.%GOARCH%
+go build -ldflags="-s -w" -o .dist/%name%.linux
 
 set "GOOS=darwin" & set "GOARCH=amd64"
-go build -ldflags="-s -w" -o .dist/%name%.%GOOS%.%GOARCH%
+go build -ldflags="-s -w" -o .dist/%name%.macos
+
+set "GOOS=android" & set "GOARCH=arm64"
+go build -ldflags="-s -w" -o .dist/%name%.android
 
 echo  * Built completed.
 
@@ -33,3 +36,4 @@ if "%~1"=="-upx" (
     echo  * Upx compressing completed.
 )
 
+popd
---
+++ golang/.dist/build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Usage:
+#   build
+#   build -upx
+
+name=dandanplay-resource-service
+
+root=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+
+pushd $root/.. >/dev/null 2>&1
+
+echo "* go building..."
+
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o .dist/$name.win64.exe
+GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o .dist/$name.win32.exe
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o .dist/$name.linux
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o .dist/$name.macos
+GOOS=android GOARCH=arm64 go build -ldflags="-s -w" -o .dist/$name.android
+
+echo "* Built completed."
+
+cd .dist
+
+if [ x$1 == x-upx ]; then
+    echo "* Upx compressing..."
+    upx --lzma --best $name.*
+    echo "* Upx compressing completed."
+fi
+
+sha256sum $name.* > sha256sum.txt
+
+popd >/dev/null 2>&1
