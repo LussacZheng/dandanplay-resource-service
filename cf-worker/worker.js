ce605f748e0ff25c07c13265f1351c1da0098629
Lussac Zheng <lussaczheng@gmail.com>
2021-02-06 16:21:46 +0800

    build(cfw): v0.0.2

--- cf-worker/worker.js
+++ cf-worker/worker.js
@@ -1,11 +1,12 @@
 // https://github.com/LussacZheng/dandanplay-resource-service
+// version: 0.0.2
 
 !(function(e) {
   var t = {}
   function n(r) {
     if (t[r]) return t[r].exports
-    var o = (t[r] = { i: r, l: !1, exports: {} })
-    return e[r].call(o.exports, o, o.exports, n), (o.l = !0), o.exports
+    var a = (t[r] = { i: r, l: !1, exports: {} })
+    return e[r].call(a.exports, a, a.exports, n), (a.l = !0), a.exports
   }
   ;(n.m = e),
     (n.c = t),
@@ -27,13 +28,13 @@
         Object.defineProperty(r, 'default', { enumerable: !0, value: e }),
         2 & t && 'string' != typeof e)
       )
-        for (var o in e)
+        for (var a in e)
           n.d(
             r,
-            o,
+            a,
             function(t) {
               return e[t]
-            }.bind(null, o),
+            }.bind(null, a),
           )
       return r
     }),
@@ -55,11 +56,27 @@
     n((n.s = 1))
 })([
   function(e) {
-    e.exports = { a: 'https://github.com/LussacZheng/dandanplay-resource-service' }
+    e.exports = { b: '0.0.2', a: 'https://github.com/LussacZheng/dandanplay-resource-service' }
   },
   function(e, t, n) {
     'use strict'
-    function r(e, t) {
+    function r(e) {
+      const t = new Date(e).toLocaleString('default', {
+        formatMatcher: 'best fit',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hourCycle: 'h23',
+      })
+      return new Date(t + ' UTC')
+        .toISOString()
+        .substr(0, 19)
+        .replace('T', ' ')
+    }
+    function a(e, t) {
       if (0 === t.length) return e[1]
       let n = {}
       return (
@@ -71,36 +88,37 @@
       )
     }
     n.r(t)
-    var o = function(e, t, n, o = 'first') {
-      switch (o) {
+    var o = function(e, t, n, r = 'first') {
+      switch (r) {
         case 'all':
           return (function(e, t, n) {
-            const o = e.matchAll(t),
-              s = Array.from(o, e => r(e, n))
-            return 0 === s.length ? null : s
+            const r = e.matchAll(t),
+              o = Array.from(r, e => a(e, n))
+            return 0 === o.length ? null : o
           })(e, t, n)
         case 'last':
           return (function(e, t, n) {
-            const o = [...e.matchAll(t)],
-              s = o[o.length - 1]
-            return 0 === o.length ? null : r(s, n)
+            const r = [...e.matchAll(t)],
+              o = r[r.length - 1]
+            return 0 === r.length ? null : a(o, n)
           })(e, t, n)
         default:
           return (function(e, t, n) {
-            let o = t.exec(e)
-            return (t.lastIndex = 0), null === o ? null : r(o, n)
+            let r = t.exec(e)
+            return (t.lastIndex = 0), null === r ? null : a(r, n)
           })(e, t, n)
       }
     }
     const s = { headers: { 'content-type': 'application/json;charset=utf-8' } },
-      a = {
+      i = { headers: { 'content-type': 'text/html;charset=utf-8' } },
+      u = {
         headers: {
           accept: 'text/html;charset=utf-8',
           'user-agent':
             'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
         },
       }
-    async function i(e, t = a) {
+    async function c(e, t = u) {
       const n = await fetch(decodeURI(e), t)
       return await (async function(e) {
         const { headers: t } = e,
@@ -110,18 +128,25 @@
           : (n.includes('application/text') || n.includes('text/html'), await e.text())
       })(n)
     }
-    const u = 'https://share.dmhy.org',
-      c = {
-        type_and_subgroup_url: u + '/topics/advanced-search?team_id=0&sort_id=0&orderby=',
+    const l = 'https://share.dmhy.org',
+      d = {
+        type_and_subgroup_url: l + '/topics/advanced-search?team_id=0&sort_id=0&orderby=',
         list_url:
-          u +
+          l +
           '/topics/list/page/1?keyword=${keyword}&sort_id=${type}&team_id=${subgroup}&order=date-desc',
-        unknown_subgroup_id: -1,
-        unknown_subgroup_name: '未知字幕组',
       },
-      l = /<option value="(\d+)">(.+?)<\/option>/gim,
-      d = /<option value="(\d+)" style="color: [\w#]+">(.+?)<\/option>/gim,
-      p = {
+      h = '未能成功解析标题',
+      p = -2,
+      g = '未能成功解析类别',
+      f = -1,
+      y = '未知字幕组',
+      m = 'magnet_not_found_未能成功解析磁力链接',
+      b = '未能成功解析资源发布页面',
+      w = '未能成功解析资源大小',
+      _ = '1970-01-05 08:00:00',
+      v = /<option value="(\d+)">(.+?)<\/option>/gim,
+      I = /<option value="(\d+)" style="color: [\w#]+">(.+?)<\/option>/gim,
+      S = {
         HasMore: /下一頁/g,
         Resources: /<tr class="">(.*?)<\/tr>/gis,
         TypeId: /href="\/topics\/list\/sort_id\/(\d+)"/gim,
@@ -135,96 +160,83 @@
         Title: /target="_blank" ?>(.+?)<\/a>/gis,
         TitleReplacer: /<span class="keyword">(.*?)<\/span>/gi,
       }
-    async function h(e) {
-      const t = new URL(e.url).searchParams,
-        n = encodeURI(
-          ((r = c.list_url),
-          (s = {
-            keyword: t.get('keyword'),
-            type: t.get('type') || 0,
-            subgroup: t.get('subgroup') || 0,
-          }),
-          r.replace(/\$\{(\w+)\}/gi, (e, t) => s[t])),
+    async function T(e) {
+      const t = new URL(encodeURI(e.url)).searchParams,
+        n = t.get('type') || 0,
+        a = t.get('subgroup') || 0,
+        s = encodeURI(
+          ((i = d.list_url),
+          (u = { keyword: t.get('keyword'), type: n < 0 ? 0 : n, subgroup: a < 0 ? 0 : a }),
+          i.replace(/\$\{(\w+)\}/gi, (e, t) => u[t])),
         )
-      var r, s
-      let a
+      var i, u
+      let v
       try {
-        a = await i(n)
+        v = await c(s)
       } catch (e) {
         console.error(e)
       }
       return (function(e) {
-        let t = { HasMore: null !== o(e, p.HasMore, []), Resources: [] }
+        let t = { HasMore: null !== o(e, S.HasMore, []), Resources: [] }
+        const n = o(e, S.Resources, [], 'all')
+        if (null === n) return t
         return (
-          o(e, p.Resources, [], 'all').forEach(e => {
+          n.forEach(e => {
             t.Resources.push(
               (function(e) {
-                const t = o(e, p.Title, []),
-                  n = o(e, p.TypeId, []),
-                  r = o(e, p.TypeName, []),
-                  s = o(e, p.SubgroupId, []) || c.unknown_subgroup_id,
-                  a = o(e, p.SubgroupName, []) || c.unknown_subgroup_name,
-                  i = o(e, p.Magnet, []),
-                  l = u + o(e, p.PageUrl, []),
-                  d = o(e, p.FileSize, []),
-                  h = (function(e) {
-                    const t = new Date(e).toLocaleString('default', {
-                      formatMatcher: 'best fit',
-                      year: 'numeric',
-                      month: '2-digit',
-                      day: '2-digit',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      second: '2-digit',
-                      hourCycle: 'h23',
-                    })
-                    return new Date(t + ' UTC')
-                      .toISOString()
-                      .substr(0, 19)
-                      .replace('T', ' ')
-                  })(o(e, p.PublishDate, []))
+                const t = o(e, S.Title, []),
+                  n = o(e, S.TypeId, []),
+                  a = o(e, S.TypeName, []),
+                  s = o(e, S.SubgroupId, []),
+                  i = o(e, S.SubgroupName, []),
+                  u = o(e, S.Magnet, []),
+                  c = l + o(e, S.PageUrl, []),
+                  d = o(e, S.FileSize, []),
+                  v = o(e, S.PublishDate, [])
                 return {
-                  Title: t.trim().replace(p.TitleReplacer, '$1'),
-                  TypeId: parseInt(n),
-                  TypeName: r,
-                  SubgroupId: parseInt(s),
-                  SubgroupName: a,
-                  Magnet: i,
-                  PageUrl: l,
-                  FileSize: d,
-                  PublishDate: h,
+                  Title: null === t ? h : t.trim().replace(S.TitleReplacer, '$1'),
+                  TypeId: parseInt(n) || p,
+                  TypeName: a || g,
+                  SubgroupId: parseInt(s) || f,
+                  SubgroupName: i || y,
+                  Magnet: u || m,
+                  PageUrl: null === c ? b : l + c,
+                  FileSize: d || w,
+                  PublishDate: null === v ? _ : r(v),
                 }
               })(e),
             )
           }),
           t
         )
-      })(a)
+      })(v)
     }
-    function g(e) {
+    function x(e) {
       const t = e.replace(/&amp;/gi, '&')
-      let n = o(t, l, ['Id', 'Name'], 'all')
-      return n.forEach(e => (e.Id = parseInt(e.Id))), n.shift(), n
+      let n = o(t, v, ['Id', 'Name'], 'all')
+      return null === n ? [] : (n.forEach(e => (e.Id = parseInt(e.Id))), n.shift(), n)
     }
-    function f(e) {
-      let t = o(e, d, ['Id', 'Name'], 'all')
-      return t.forEach(e => (e.Id = parseInt(e.Id))), t.unshift({ Id: 0, Name: '全部' }), t
+    function R(e) {
+      let t = o(e, I, ['Id', 'Name'], 'all')
+      return null === t
+        ? []
+        : (t.forEach(e => (e.Id = parseInt(e.Id))), t.unshift({ Id: 0, Name: '全部' }), t)
     }
-    const y = e => t => t.method.toLowerCase() === e.toLowerCase(),
-      m = y('connect'),
-      b = y('delete'),
-      w = y('get'),
-      _ = y('head'),
-      I = y('options'),
-      S = y('patch'),
-      v = y('post'),
-      T = y('put'),
-      x = y('trace'),
-      R = e => t => {
-        const n = new URL(t.url).pathname
+    const N = e => t => t.method.toLowerCase() === e.toLowerCase(),
+      P = N('connect'),
+      M = N('delete'),
+      O = N('get'),
+      j = N('head'),
+      U = N('options'),
+      L = N('patch'),
+      k = N('post'),
+      $ = N('put'),
+      A = N('trace'),
+      C = e => t => {
+        const n = new URL(encodeURI(t.url)).pathname
         return (n.match(e) || [])[0] === n
       }
-    var N = class {
+    var D = class {
         constructor() {
           this.routes = []
         }
@@ -232,31 +244,31 @@
           return this.routes.push({ conditions: e, handler: t }), this
         }
         connect(e, t) {
-          return this.handle([m, R(e)], t)
+          return this.handle([P, C(e)], t)
         }
         delete(e, t) {
-          return this.handle([b, R(e)], t)
+          return this.handle([M, C(e)], t)
         }
         get(e, t) {
-          return this.handle([w, R(e)], t)
+          return this.handle([O, C(e)], t)
         }
         head(e, t) {
-          return this.handle([_, R(e)], t)
+          return this.handle([j, C(e)], t)
         }
         options(e, t) {
-          return this.handle([I, R(e)], t)
+          return this.handle([U, C(e)], t)
         }
         patch(e, t) {
-          return this.handle([S, R(e)], t)
+          return this.handle([L, C(e)], t)
         }
         post(e, t) {
-          return this.handle([v, R(e)], t)
+          return this.handle([k, C(e)], t)
         }
         put(e, t) {
-          return this.handle([T, R(e)], t)
+          return this.handle([$, C(e)], t)
         }
         trace(e, t) {
-          return this.handle([x, R(e)], t)
+          return this.handle([A, C(e)], t)
         }
         all(e) {
           return this.handle([], e)
@@ -279,18 +291,18 @@
           )
         }
       },
-      k = n(0)
-    async function M(e) {
-      const t = new N()
+      E = n(0)
+    async function z(e) {
+      const t = new D()
       t.get('/subgroup', async () => {
         const e = await (async function() {
           let e
           try {
-            e = await i(c.type_and_subgroup_url)
+            e = await c(d.type_and_subgroup_url)
           } catch (e) {
             console.error(e)
           }
-          return { Subgroups: g(e) }
+          return { Subgroups: x(e) }
         })()
         return new Response(JSON.stringify(e), s)
       }),
@@ -298,21 +310,39 @@
           const e = await (async function() {
             let e
             try {
-              e = await i(c.type_and_subgroup_url)
+              e = await c(d.type_and_subgroup_url)
             } catch (e) {
               console.error(e)
             }
-            return { Types: f(e) }
+            return { Types: R(e) }
           })()
           return new Response(JSON.stringify(e), s)
         }),
         t.get('/list', async e => {
-          const t = await h(e)
+          const t = await T(e)
           return new Response(JSON.stringify(t), s)
         }),
-        t.get('/', () => new Response('使用说明：' + k.a))
+        t.get(
+          '/',
+          async () =>
+            new Response(
+              await (async function() {
+                let e
+                try {
+                  e = await c(
+                    'https://cdn.jsdelivr.net/gh/LussacZheng/dandanplay-resource-service@dist/web/index.html',
+                  )
+                } catch (t) {
+                  console.error(t),
+                    (e = `\n<!DOCTYPE html>\n<html lang="zh-CN">\n<head>\n  <meta charset="UTF-8" />\n  <meta name="viewport" content="width=device-width,initial-scale=1" />\n  <title>弹弹play资源搜索节点API - v${E.b}</title>\n</head>\n<body>\n  <h1>使用说明</h1>\n  <h2>GitHub - <a href="${E.a}">LussacZheng/dandanplay-resource-service</a></h2>\n</body>\n</html>\n`)
+                }
+                return e
+              })(),
+              i,
+            ),
+        )
       return await t.route(e)
     }
-    addEventListener('fetch', e => e.respondWith(M(e.request)))
+    addEventListener('fetch', e => e.respondWith(z(e.request)))
   },
 ])
